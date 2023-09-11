0a81ec4748bf174bb6477ef3f2834bc728061ab5
shrutika546 <84497284+shrutika546@users.noreply.github.com>
2023-09-11 16:51:27 +0530

    Initial commit

---
+++ Django_final_project/signup/myapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class user_model(models.Model):
+    username=models.CharField(max_length=20)
+    email = models.EmailField()
+    password1 = models.CharField(max_length=20)
+    password2 = models.CharField(max_length=20)
